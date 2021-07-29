cb396d6f3db218f0514ad53dc14b775779516eef
Kizito Akwotiga <kakwotiga@yahoo.com>
2021-07-28 21:50:20 -0400

    assignment 7 update

--- assignment7.js
+++ assignment7.js
@@ -1,9 +1,13 @@
+console.log("...................A...............");
+
 const cel = 0;
 const far = 32;
 const celToFar = (cel*9/5)+32;
 const farToCel = (far-32)*5/9;
 console.log(`${cel}ºC is ${celToFar}ºF`);
 console.log(`${far}ºF is ${farToCel}ºC`)
+
+console.log("...................B...............");
 
 let lucasHeight= 1.95;
 let lucasWeight= 1.09;
@@ -22,14 +26,16 @@ if(lucasBMI > johnBMI){
 else{
     console.log(`John's BMI (${johnBMI}) is higher than Lucas' (${lucasBMI})`)
 }
+console.log("...................C...............");
 
-const netsScore1 = 96;
-const netsScore2 = 108;
-const netsScore3 = 89;
+console.log(" C: Data 1");
+let netsScore1 = 96;
+let netsScore2 = 108;
+let netsScore3 = 89;
 
-const knicksScore1 = 88;
-const knicksScore2 = 91;
-const knicksScore3 = 110;
+let knicksScore1 = 88;
+let knicksScore2 = 91;
+let knicksScore3 = 110;
 
 let netsAverage =(netsScore1 + netsScore2 + netsScore3) / 3;
 console.log("Nets Average = " + netsAverage);
@@ -44,6 +50,40 @@ if(netsAverage > knicksAverage){
 }else{
     console.log(" it is a draw")
 }
+
+console.log("Data Bonus 1");
+netsScore1 = 97;
+netsScore2 = 112;
+netsScore3 = 101;
+
+knicksScore1 = 109;
+knicksScore2 = 95;
+knicksScore3 = 123;
+
+netsAverage =(netsScore1 + netsScore2 + netsScore3) / 3;
+console.log("Nets Average = " + netsAverage);
+
+knicksAverage =(knicksScore1 + knicksScore2 + knicksScore3) /3;
+console.log("Knicks Average = " + knicksAverage); 
+
+if(netsAverage>knicksAverage && netsAverage>99){
+    console.log(`netsAverage wins with ${netsAverage} points`)
+}else if(knicksAverage>netsAverage && knicksAverage>99){
+    console.log(`knicksAverage wins with ${knicksAverage} points`)
+}else if(netsAverage == knicksAverage && netsAverage>99){
+    console.log("this is a draw")
+}else{
+    console.log("No win No draw No Trophy")
+}
+
+console.log("Data Bonus 2");
+knicksScore3 = 106;
+
+netsAverage =(netsScore1 + netsScore2 + netsScore3) / 3;
+console.log("Nets Average = " + netsAverage);
+
+knicksAverage =(knicksScore1 + knicksScore2 + knicksScore3) /3;
+console.log("Knicks Average = " + knicksAverage); 
 
 if(netsAverage>knicksAverage && netsAverage>99){
     console.log(`netsAverage wins with ${netsAverage} points`)
